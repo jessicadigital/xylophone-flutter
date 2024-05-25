f765602a3647a6c4c2b8028f1226d19af57846d5
Dr Jessica Muirhead <github@jessica.digital>
2024-05-25 19:40:36 +0100

    Flutter Bootcamp: Lesson 79 Updating the UI

--- lib/main.dart
+++ lib/main.dart
@@ -10,90 +10,37 @@ class XylophoneApp extends StatelessWidget {
     await player.play(AssetSource('note$soundNumber.wav'));
   }
 
+  Widget buildKey() {
+    return Expanded(
+      child: TextButton(
+        style: const ButtonStyle(
+          backgroundColor: WidgetStatePropertyAll(Colors.red),
+        ),
+        onPressed: () async {
+          await playSound(1);
+        },
+        child:Container(),
+      )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Column(children: <Widget>[
-            TextButton(
-              style: ButtonStyle(
-                backgroundColor: WidgetStatePropertyAll(Colors.red),
-              ),
-              onPressed: () async {
-                await playSound(1);
-              },
-              child: Text(
-                '',
-              ),
-            ),
-            TextButton(
-              style: ButtonStyle(
-                backgroundColor: WidgetStatePropertyAll(Colors.orange),
-              ),
-              onPressed: () async {
-                await playSound(2);
-              },
-              child: Text(
-                '',
-              ),
-            ),
-            TextButton(
-              style: ButtonStyle(
-                backgroundColor: WidgetStatePropertyAll(Colors.yellow),
-              ),
-              onPressed: () async {
-                await playSound(3);
-              },
-              child: Text(
-                '',
-              ),
-            ),
-            TextButton(
-              style: ButtonStyle(
-                backgroundColor: WidgetStatePropertyAll(Colors.green),
-              ),
-              onPressed: () async {
-                await playSound(4);
-              },
-              child: Text(
-                '',
-              ),
-            ),
-            TextButton(
-              style: ButtonStyle(
-                backgroundColor: WidgetStatePropertyAll(Colors.teal),
-              ),
-              onPressed: () async {
-                await playSound(5);
-              },
-              child: Text(
-                '',
-              ),
-            ),
-            TextButton(
-              style: ButtonStyle(
-                backgroundColor: WidgetStatePropertyAll(Colors.blue),
-              ),
-              onPressed: () async {
-                await playSound(6);
-              },
-              child: Text(
-                '',
-              ),
-            ),
-            TextButton(
-              style: ButtonStyle(
-                backgroundColor: WidgetStatePropertyAll(Colors.purple),
-              ),
-              onPressed: () async {
-                await playSound(7);
-              },
-              child: Text(
-                '',
-              ),
-            ),
-          ]),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                buildKey(),
+                buildKey(),
+                buildKey(),
+                buildKey(),
+                buildKey(),
+                buildKey(),
+                buildKey(),
+              ]),
         ),
       ),
     );
