588b7d704574484c0417216b99cf1100d734caf6
Dr Jessica Muirhead <github@jessica.digital>
2024-05-25 19:30:31 +0100

    Flutter Bootcamp: Lesson 77 Play Multiple Sounds

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,96 @@ import 'package:flutter/material.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  final player = AudioPlayer();
+
+  Future<void> playSound(int soundNumber) async {
+    await player.play(AssetSource('note$soundNumber.wav'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
+          child: Column(children: <Widget>[
+            TextButton(
+              style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(Colors.red),
+              ),
               onPressed: () async {
-                final player = AudioPlayer();
-                await player.play(AssetSource('note1.wav'));
+                await playSound(1);
               },
-              child: Text('Click Me',),
+              child: Text(
+                '',
+              ),
             ),
-          ),
+            TextButton(
+              style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(Colors.orange),
+              ),
+              onPressed: () async {
+                await playSound(2);
+              },
+              child: Text(
+                '',
+              ),
+            ),
+            TextButton(
+              style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(Colors.yellow),
+              ),
+              onPressed: () async {
+                await playSound(3);
+              },
+              child: Text(
+                '',
+              ),
+            ),
+            TextButton(
+              style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(Colors.green),
+              ),
+              onPressed: () async {
+                await playSound(4);
+              },
+              child: Text(
+                '',
+              ),
+            ),
+            TextButton(
+              style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(Colors.teal),
+              ),
+              onPressed: () async {
+                await playSound(5);
+              },
+              child: Text(
+                '',
+              ),
+            ),
+            TextButton(
+              style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(Colors.blue),
+              ),
+              onPressed: () async {
+                await playSound(6);
+              },
+              child: Text(
+                '',
+              ),
+            ),
+            TextButton(
+              style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(Colors.purple),
+              ),
+              onPressed: () async {
+                await playSound(7);
+              },
+              child: Text(
+                '',
+              ),
+            ),
+          ]),
         ),
       ),
     );
